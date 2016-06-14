d2b9b1212d3308b7da3f90817d6ff36de1854446
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-06-14 13:33:26 +0200

    refactoring + handle clicks on nodes

--- app/scripts/network1.js
+++ app/scripts/network1.js
@@ -1,42 +1,63 @@
+var container = document.getElementById('network1');
+
+var data = null;
+
+var parserOptions = {
+  edges: {
+    inheritColors: false
+  },
+  nodes: {
+    fixed: true,
+    parseColor: false
+  }
+}
+
+var options = {
+  "nodes": {
+    "shape": "box",
+    "font": {
+      "color": "black"
+    }
+  },
+  "edges": {
+    "font": {
+      "color": "white",
+  	  "strokeWidth": 0
+  	}
+  },
+  "interaction": {
+    "hover": true
+  },
+  "physics": {
+    "stabilization": {
+      "enabled": false
+    }
+  }
+}
+
 $.getJSON("data/rules.json", function( gephiJSON ) {
-	var parserOptions = {
-	  edges: {
-	    inheritColors: false
-	  },
-	  nodes: {
-	    fixed: true,
-	    parseColor: false
-	  }
-	}
+	
 	var parsed = vis.network.convertGephi(gephiJSON, parserOptions);
 
-	var data = {
+	data = {
 	  nodes: parsed.nodes,
 	  edges: parsed.edges
 	};
-	var options = {
-	  "nodes": {
-	    "shape": "box",
-	    "font": {
-	    	"color": "black"
-	    }
-	  },
-	  "edges": {
-	  	"font": {
-	  		"color": "white",
-	  		"strokeWidth": 0
-	  	}
-	  },
-	  "interaction": {
-            "hover": true
-      },
-	"physics": {
-		"stabilization": {
-	      "enabled": false
-	    }
-	  }
-	}
 
-	var container = document.getElementById('network1');
 	var network = new vis.Network(container, data, options);
+	network.on("selectNode", handleSelectNode);
 });
+
+handleSelectNode = function( obj ) {
+	nodeId = obj.nodes[0];
+	node = getNode(nodeId);
+	alert(node.label);
+}
+
+getNode = function( nodeId ) {
+	for(var i=0, l=data.nodes.length; i<l; i++) {
+		if(data.nodes[i].id == nodeId) {
+			return data.nodes[i];
+		}
+	}
+}
