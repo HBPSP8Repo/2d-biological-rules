7e99895aa5dc2d2215bc78ba329186696e7ddcf8
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-06-14 09:06:59 +0200

    set nodes shape to box

--- app/scripts/network1.js
+++ app/scripts/network1.js
@@ -14,7 +14,11 @@ $.getJSON("data/rules.json", function( gephiJSON ) {
 	  nodes: parsed.nodes,
 	  edges: parsed.edges
 	};
-	var options = {};
+	var options = {
+	  "nodes": {
+	    "shape": "box"
+	  }
+	}
 
 	var container = document.getElementById('network1');
 	var network = new vis.Network(container, data, options);
