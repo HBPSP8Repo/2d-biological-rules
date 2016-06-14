5fa70957d428422540d039362e638f15d5b9a150
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-06-14 15:38:52 +0200

    add information panel with external resource links

--- app/scripts/network1.js
+++ app/scripts/network1.js
@@ -51,7 +51,55 @@ $.getJSON("data/rules.json", function( gephiJSON ) {
 handleSelectNode = function( obj ) {
 	nodeId = obj.nodes[0];
 	node = getNode(nodeId);
-	alert(node.label);
+	label = node.label;
+	color = node.color.background;
+	
+	$("#noInfoText").remove();
+	$("#nodeInfoText").empty();
+	$("#nodeInfoText").append("Name: "+label+"<br/>")
+
+    var url = undefined;
+    if(color == 'rgb(255,255,128)')
+    {
+    	// Genetic
+        url = 'http://www.genecards.org/cgi-bin/carddisp.pl?gene=' + encodeURIComponent(label.match("\\[(.*)\\]")[1]);
+    }
+    else if (color == 'rgb(128,255,128)')
+    {
+    	// Brain Anatomy
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else if (color == 'rgb(255,153,0)')
+    {
+    	// Brain Metabolism
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else if (color == 'rgb(255,128,128)')
+    {
+    	// AD
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else if (color == 'rgb(0,102,255)')
+    {
+    	// Control
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else if (color == 'rgb(0,204,204)')
+    {
+    	// CSF Protein
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else if (color == 'rgb(204,0,255)')
+    {
+    	// Blood Protein
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);
+    }
+    else
+    {
+    	// Unknown
+        url = 'http://google.com/search?q=' + encodeURIComponent(label);   
+    }
+    $("#nodeInfoText").append("<a target=\"_blank\" href=\""+url+"\">More...<a/>")
 }
 
 getNode = function( nodeId ) {
